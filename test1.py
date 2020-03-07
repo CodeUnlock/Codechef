f84eee4a720f99633164ce1e0b1303b52aa2940e
UtkarshSao <34209762+Utkarsh-Sao-mvj@users.noreply.github.com>
2020-03-07 23:14:53 +0530

    ..

--- test1.py
+++ test1.py
@@ -1,1 +1,13 @@
-print("hhhhhhh")
+for _ in range(int(input())):
+    n,m = [int(i) for i in input().split()]
+    a = [int (i) for i in input().split()]
+    b = [int (i) for i in input().split()]
+    temp = []
+    for i in range(1,m+1):
+        sum = 0
+        for j in range(n):
+            if(i==a[j]):
+                sum=sum+b[j]
+        if(sum!=0):
+            temp.append(sum)
+    print(min(temp))
